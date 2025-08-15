d1cd38e8eb8180d3d6e771835e4b83856b3e8d92
mithu1505 <mithur0515@gmail.com>
2025-08-15 20:48:32 +0530

    Update AdminRegister.jsx
    corrected the error

--- frontend/src/pages/AdminRegister.jsx
+++ frontend/src/pages/AdminRegister.jsx
@@ -18,7 +18,9 @@ const AdminRegister = () => {
         push: (path) => console.log(`Navigating to: ${path}`)
     };
 
-  
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
