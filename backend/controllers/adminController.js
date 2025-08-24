9730d4e250c9912bf4047e9c71ec9c3d956a1de0
Lathurjan Thavapiragasam <165404387+Lathu2001@users.noreply.github.com>
2025-08-24 17:18:02 +0530

    fixed bug

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 const Admin = require('../models/Admin');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
